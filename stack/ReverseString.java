8a2f1cddc45e03dc0c1de7cd9ae4b85233735136
João Luis Moraes <jonmoraesnl@gmail.com>
2021-11-09 10:50:27 -0300

    next greater

--- stack/ReverseString.java
+++ stack/ReverseString.java
@@ -20,9 +20,31 @@ public class ReverseString {
     return new String(chars);
   }
 
+  public static int[] nextGreaterElement(int[] arr) {
+    int[] result = new int[arr.length];
+    Stack<Integer> stack = new Stack<>();
+
+    for (int i = arr.length - 1; i >= 0; i--) {
+      if (!stack.isEmpty()) {
+        while (!stack.isEmpty() && stack.peek() <= arr[i]) {
+          stack.pop();
+        }
+      }
+
+      if (stack.isEmpty()) {
+        result[i] = -1;
+      } else {
+        result[i] = stack.peek();
+      }
+      stack.push(arr[i]);
+    }
+
+    System.out.println(result);
+    return result;
+  }
+
   public static void main(String[] args) {
     String str = "JOAO";
-
     System.out.println(reverse(str));
   }
 }
